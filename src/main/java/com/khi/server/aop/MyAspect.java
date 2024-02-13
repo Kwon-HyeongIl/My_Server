a2dcd6dc124f92b70e89f89bd91ac9a1a223d373
khi <llioopv@naver.com>
2024-02-13 23:57:01 +0900

    update security, main logic

--- src/main/java/com/khi/server/aop/MyAspect.java
+++ src/main/java/com/khi/server/aop/MyAspect.java
@@ -1,19 +1,21 @@
-package com.khi.server.aop.aspect;
+package com.khi.server.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect
+@Component
 public class MyAspect {
 
     /*
      * 포인트컷 지정에서 AopConfig 클래스를 제외하지 않을 경우, 자기 자신인 LogAspect를 생성하는 메서드도 Aop로 처리하게 되므로, 순환 참조에 빠지게 됨
-     * (+LogAspect에 @Component 어노테이션을 붙여서 컴포넌트 스캔으로 빈을 등록해도 문제가 해결됨)
+     * (+MyAspect에 @Component 어노테이션을 붙여서 컴포넌트 스캔으로 빈을 등록해도 문제가 해결됨)
      */
-    @Around("execution(* com.khi.server..*(..)) && !target(com.khi.server.aop.configuration.AopConfig)")
+    @Around("execution(* com.khi.server..*(..))")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
 
         String[] fullLogArr = String.valueOf(joinPoint.getSignature()).split("\\.");
