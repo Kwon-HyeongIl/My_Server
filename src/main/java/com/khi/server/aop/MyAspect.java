94936341010c350c6511c768edf6e0d555c3c57d
khi <llioopv@naver.com>
2024-02-14 14:10:15 +0900

    update security, main logic

--- src/main/java/com/khi/server/aop/MyAspect.java
+++ src/main/java/com/khi/server/aop/MyAspect.java
@@ -10,19 +10,20 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyAspect {
-
     /*
-     * 포인트컷 지정에서 AopConfig 클래스를 제외하지 않을 경우, 자기 자신인 LogAspect를 생성하는 메서드도 Aop로 처리하게 되므로, 순환 참조에 빠지게 됨
+     * Configuration을 따로 구성한 상태의 포인트컷 지정에서 AopConfig 클래스를 제외하지 않을 경우,
+     * 자기 자신인 LogAspect를 생성하는 메서드도 Aop로 처리하게 되므로, 순환 참조에 빠지게 됨
      * (+MyAspect에 @Component 어노테이션을 붙여서 컴포넌트 스캔으로 빈을 등록해도 문제가 해결됨)
      */
+
     @Around("execution(* com.khi.server..*(..))")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
 
         String[] fullLogArr = String.valueOf(joinPoint.getSignature()).split("\\.");
-        String classType = fullLogArr[fullLogArr.length-2];
-        String methodType = fullLogArr[fullLogArr.length-1].split("\\(")[0];
+        String className = fullLogArr[fullLogArr.length-2];
+        String methodName = fullLogArr[fullLogArr.length-1].split("\\(")[0];
 
-        log.info("[MyLog] " + "(" + classType + ") " + methodType + " 메서드 호출");
+        log.info("[MyLog] " + "(" + className + ") " + methodName + " 메서드 호출");
 
         return joinPoint.proceed();
     }
